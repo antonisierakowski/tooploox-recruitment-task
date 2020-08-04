7de8813778fee3ebc0d2a116e0b2d8bc614fbd70
antonisierakowski <antonisierakowski@gmail.com>
2020-08-04 13:51:18 +0200

    Add HttpClient

---
+++ src/services/httpClient/index.ts
@@ -0,0 +1,62 @@
+import * as Axios from 'axios';
+import * as exceptions from './exceptions';
+import { StatusCode } from './types';
+
+export const __axiosInstance = Axios.default.create({
+  baseURL: GITHUB_BASE_DOMAIN,
+});
+
+export class HttpClient {
+  private static instance: HttpClient;
+  private axiosInstance: Axios.AxiosInstance;
+
+  private constructor() {
+    this.axiosInstance = __axiosInstance;
+  }
+
+  static getInstance(): HttpClient {
+    if (!HttpClient.instance) {
+      HttpClient.instance = new HttpClient();
+    }
+
+    return HttpClient.instance;
+  }
+
+  private throwBasedOnStatusCode(response: Axios.AxiosResponse) {
+    switch (response.status) {
+      case StatusCode.OK: {
+        return response;
+      }
+      case StatusCode.UNPROCESSABLE_ENTITY: {
+        throw new exceptions.ApiValidationError();
+      }
+      case StatusCode.RESOURCE_NOT_FOUND: {
+        throw new exceptions.ResourceNotFoundError();
+      }
+      case StatusCode.FORBIDDEN: {
+        throw new exceptions.ForbiddenError();
+      }
+      case StatusCode.BAD_REQUEST: {
+        throw new exceptions.BadRequestError();
+      }
+      case StatusCode.INTERNAL_ERROR:
+      default: {
+        throw new exceptions.ApiInternalError();
+      }
+    }
+  }
+
+  async get<TResponse = any>(url: string): Promise<TResponse> {
+    try {
+      const response = await this.axiosInstance.get(url);
+      return response.data;
+    } catch (error) {
+      if (!error.response) {
+        throw new exceptions.NoApiResponseError();
+      }
+      this.throwBasedOnStatusCode(error.response);
+    }
+  }
+}
+
+export default HttpClient.getInstance();
